413acb9d1cc8592d159a6805ebca137879056f9c
Chris Threadgill <christhreadgill@live.com>
2022-05-05 17:29:34 -0500

    default profile pictures crashing heroku server?

--- backend/db/seeders/20220429042736-demo-users.js
+++ backend/db/seeders/20220429042736-demo-users.js
@@ -18,6 +18,7 @@ module.exports = {
           email: "demo@user.io",
           hashedPassword: bcrypt.hashSync("password"),
           roleId: 1,
+          profilePicture: "aa32be9f0cf08a175725c2748989a3a1",
         },
         {
           firstName: "Ara",
@@ -25,6 +26,7 @@ module.exports = {
           email: "user1@user.io",
           hashedPassword: bcrypt.hashSync("password2"),
           roleId: 1,
+          profilePicture: "aa32be9f0cf08a175725c2748989a3a1",
         },
         {
           firstName: "JP",
@@ -32,6 +34,7 @@ module.exports = {
           email: "user2@user.io",
           hashedPassword: bcrypt.hashSync("password3"),
           roleId: 2,
+          profilePicture: "aa32be9f0cf08a175725c2748989a3a1",
         },
         {
           firstName: "Darren",
@@ -39,6 +42,7 @@ module.exports = {
           email: "user3@user.io",
           hashedPassword: bcrypt.hashSync("password4"),
           roleId: 2,
+          profilePicture: "aa32be9f0cf08a175725c2748989a3a1",
         },
       ],
       {}
